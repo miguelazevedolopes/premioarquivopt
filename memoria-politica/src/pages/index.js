6628129bcb14cd35d5622cad63ec6f06f2de00e7
Miguel Lopes <lopesazevedom@gmail.com>
2023-04-23 23:21:12 +0100

    added wordcloud

--- memoria-politica/src/pages/index.js
+++ memoria-politica/src/pages/index.js
@@ -1,21 +1,47 @@
 import Head from 'next/head'
+import { TagCloud } from 'react-tagcloud' // esta foi a unica biblioteca de wordclouds que consegui meter a funcionar
+// se arranjarem uma melhor aceito
 
 
 export default function Home() {
+  const data = [
+    {
+      value: 'JavaScript', count: 38,
+      props: {
+        title: 38, // quando se dá hover aparece o número de ocorrencias
+        style: {
+          color: 'white',
+        },
+      },
+    },
+    { value: 'React', count: 30 },
+    { value: 'Nodejs', count: 28 },
+    { value: 'Express.js', count: 25 },
+    { value: 'HTML5', count: 33 },
+    { value: 'MongoDB', count: 18 },
+    { value: 'CSS3', count: 20 }
+  ]
+
+
   return (
     <>
       <Head>
-        <title>Memória Política</title>
+        <title >Memória Política</title>
         <meta name="description" content="Um arquivo das páginas web dos partidos políticos Portugueses." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='mt-5'>
         <div className='flex-col m-auto'>
-          <h1 className='text-5xl lg:text-7xl font-extrabold inline-block'>Memória Política</h1>
-          <h3 className='text-2xl mt-0'>Descriçãozinha mini do projeto, frase impactful.</h3>
+          <h1 className='text-5xl lg:text-7xl font-extrabold inline-block select-none'>Memória Política</h1>
+          <h3 className='text-2xl mt-0 select-none'>Descriçãozinha mini do projeto, frase impactful.</h3>
           <div className='flex justify-center items-center h-80 w-100 bg-black mt-10'>
-            <h2 className='text-white text-center'>Aqui é para meter uma wordcloud geral?</h2>
+            <TagCloud
+              minSize={12}
+              maxSize={35}
+              tags={data}
+              onClick={tag => alert(`'${tag.value}' was selected!`)}
+            />
           </div>
         </div>
 
--- memoria-politica/src/pages/navbar.js
+++ memoria-politica/src/pages/navbar.js
@@ -4,10 +4,10 @@ export default function Navbar() {
   return (
     <>
       <ul className="flex justify-end p-6">
-        <li className="mr-6 p-7">
+        <li className="mr-0 p-7">
           <a className="font-bold text-xl" href="#">Partidos Políticos</a>
         </li>
-        <li className="mr-6 p-7">
+        <li className="mr-10 p-7">
           <a className="font-bold text-xl" href="#">Procure a verdade</a>
         </li>
       </ul>
