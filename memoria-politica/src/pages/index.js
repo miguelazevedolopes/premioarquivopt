21c957a86eea9b480ba8ad47cdf9f6e587da339b
Maria Carneiro <mariajcarneiro56@gmail.com>
2023-04-24 17:01:56 +0100

    prettier wordcloud

--- memoria-politica/src/pages/index.js
+++ memoria-politica/src/pages/index.js
@@ -1,82 +1,45 @@
 import Head from 'next/head'
-import { TagCloud } from 'react-tagcloud' // esta foi a unica biblioteca de wordclouds que consegui meter a funcionar
+import {useState, useEffect, useCallback } from 'react'
+import WordCloud from 'react-d3-cloud'
+import { scaleOrdinal } from 'd3-scale'
+import { schemeCategory10 } from 'd3-scale-chromatic'
+// esta foi a unica biblioteca de wordclouds que consegui meter a funcionar
 // se arranjarem uma melhor aceito
 
-
 export default function Home() {
-  const data = [
-    { value: 'JavaScript', count: 38 },
-    { value: 'React', count: 30 },
-    { value: 'Nodejs', count: 28 },
-    { value: 'Express.js', count: 25 },
-    { value: 'HTML5', count: 33 },
-    { value: 'MongoDB', count: 18 },
-    { value: 'CSS3', count: 20 },
-    { value: 'Angular', count: 22 },
-    { value: 'Vue.js', count: 17 },
-    { value: 'Python', count: 27 },
-    { value: 'Django', count: 15 },
-    { value: 'Flask', count: 12 },
-    { value: 'Ruby', count: 8 },
-    { value: 'Rails', count: 10 },
-    { value: 'PHP', count: 18 },
-    { value: 'Laravel', count: 14 },
-    { value: 'Symfony', count: 7 },
-    { value: 'Java', count: 20 },
-    { value: 'Spring', count: 16 },
-    { value: 'Hibernate', count: 9 },
-    { value: 'C++', count: 14 },
-    { value: 'C#', count: 13 },
-    { value: 'ASP.NET', count: 11 },
-    { value: 'TypeScript', count: 22 },
-    { value: 'AngularJS', count: 9 },
-    { value: 'Ember.js', count: 7 },
-    { value: 'jQuery', count: 16 },
-    { value: 'Bootstrap', count: 19 },
-    { value: 'SASS', count: 11 },
-    { value: 'LESS', count: 7 },
-    { value: 'PostgreSQL', count: 13 },
-    { value: 'MySQL', count: 15 },
-    { value: 'SQLite', count: 7 },
-    { value: 'Oracle', count: 6 },
-    { value: 'Firebase', count: 8 },
-    { value: 'AWS', count: 9 },
-    { value: 'Docker', count: 12 },
-    { value: 'Kubernetes', count: 7 },
-    { value: 'Git', count: 21 },
-    { value: 'GitHub', count: 18 },
-    { value: 'Bitbucket', count: 6 },
-    { value: 'Jenkins', count: 6 },
-    { value: 'Travis CI', count: 4 },
-    { value: 'CircleCI', count: 3 },
-    { value: 'Jest', count: 8 },
-    { value: 'Mocha', count: 7 },
-    { value: 'Chai', count: 5 },
-    { value: 'Selenium', count: 6 },
-    { value: 'JIRA', count: 5 },
-    { value: 'Trello', count: 7 },
-    { value: 'Slack', count: 9 },
-    { value: 'Zoom', count: 10 },
-    { value: 'VS Code', count: 23 },
-    { value: 'Sublime Text', count: 7 },
-    { value: 'Atom', count: 6 },
-    { value: 'Eclipse', count: 4 },
-    { value: 'IntelliJ IDEA', count: 11 },
-    { value: 'WebStorm', count: 8 },
-    { value: 'Visual Studio', count: 10 },];
+  const [isClient, setIsClient] = useState(false)
 
-  data.forEach(elem => {
-    elem.props = {
-      title: elem.count, // quando se dá hover aparece o número de ocorrencias
-      style: {
-        'color': 'white',
-        'font-weight': '400',
-        'user-select': 'none',
-        'cursor': 'pointer',
-      },
-      className:"opacity-hover"
-    };
-  })
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  const data = [
+    { text: 'Democracia', value: 7000 },
+    { text: 'Parlamento', value: 4500 },
+    { text: 'Eleições', value: 100 },
+    { text: 'Governo', value: 500 },
+    { text: 'Constituição', value: 3000 },
+    { text: 'Cidadania', value: 5000 },
+    { text: 'Direitos', value: 2000 },
+    { text: 'Deveres', value: 2000 },
+    { text: 'Justiça', value: 1000 },
+    { text: 'Transparência', value: 1000 },
+    { text: 'Corrupção', value: 200 },
+    { text: 'Liberdade', value: 2000 },
+    { text: 'Segurança', value: 500 },
+    { text: 'Orçamento', value: 400 },
+    { text: 'Política', value: 3000 }
+    ];
+
+  const onWordClick = useCallback((word) => {
+    console.log(`onWordClick: ${word}`);
+  }, []);
+  const onWordMouseOver = useCallback((word) => {
+    console.log(`onWordMouseOver: ${word}`);
+  }, []);
+  const onWordMouseOut = useCallback((word) => {
+    console.log(`onWordMouseOut: ${word}`);
+  }, []);
 
   return (
     <>
@@ -90,14 +53,9 @@ export default function Home() {
         <div className='flex-col m-auto'>
           <a href='/' className='text-5xl lg:text-7xl font-extrabold inline-block select-none'>Memória Política</a>
           <h3 className='text-2xl mt-0 select-none'>Descriçãozinha mini do projeto, frase impactful.</h3>
-          <div className='flex justify-center items-center bg-black h-80 w-100 mt-10'>
-            <TagCloud
-              style={{ width: '80%', textAlign: 'center' }}
-              minSize={7}
-              maxSize={50}
-              tags={data}
-              onClick={tag => alert(`'${tag.value}' was selected!`)}
-            />
+          <div className='bg-black h-100 w-100 mt-10'>
+            { isClient && <WordCloud data={data} height={200} font='__Epilogue_dfeb11' fontWeight="bold" spiral='rectangular'
+                            rotate='0' fill='white' onWordClick={onWordClick} onWordMouseOver={onWordMouseOver} onWordMouseOut={onWordMouseOut}  /> }
           </div>
         </div>
 
