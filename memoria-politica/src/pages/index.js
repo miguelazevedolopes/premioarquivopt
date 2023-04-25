0693ef646f1bf9061e6fa82548cad5a0aaef9e5f
Miguel Lopes <lopesazevedom@gmail.com>
2023-04-25 18:59:14 +0100

    removing dark mode css

--- memoria-politica/src/pages/index.js
+++ memoria-politica/src/pages/index.js
@@ -150,7 +150,7 @@ export default function Home() {
             <h2 className='text-3xl lg:text-4xl font-extrabold inline-block select-none mt-10'>Procure a verdade</h2>
             <h3 className='text-2xl mt-2 mb-10 select-none'>É preciso meter isto bonitinho.</h3>
 
-            <input type="search" name="" id="" className='bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-gray-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+            <input type="search" name="" id="" className='bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-gray-500 block w-full pl-10 p-2.5' />
           </div>
 
         </div>
