68d100f08df5beca9562d1b2c181ab920561940c
Miguel Lopes <lopesazevedom@gmail.com>
2023-04-25 18:21:07 +0100

    ps logo

--- memoria-politica/src/pages/navbar.js
+++ memoria-politica/src/pages/navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   return (
     <>
-      <ul className="flex justify-end p-6 relative">
+      <ul className="sticky top-0 z-50 bg-white flex justify-end p-6 relative">
         <li className="mr-0 p-7">
           <button id="dropdownParties" data-dropdown-toggle="dropdownParties" onClick={toggleDropdown} className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 md:w-auto dark:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent font-bold"> Partidos Políticos <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
             <div id="dropdownParties" className={`fixed z-10 font-normal bg-white divide-y divide-gray-100 shadow w-44 dark:bg-gray-700 dark:divide-gray-600 ${
