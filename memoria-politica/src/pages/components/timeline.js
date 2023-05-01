87d976ec1f5d035c569f8de7f56c3142d6d2eada
Miguel Lopes <lopesazevedom@gmail.com>
2023-05-01 16:37:39 +0100

    timeline css fix

--- memoria-politica/src/pages/components/timeline.js
+++ memoria-politica/src/pages/components/timeline.js
@@ -71,16 +71,21 @@ const Timeline = ({ width, height }) => {
   const eventDivs = []
   for (let index = 0; index < events.length; index++) {
     const event = events[index];
-    const yOffset = (-1 * index % 2)
+    const yOffset = -1 * (index % 2)
 
     let style = {
       height: `${Math.round(height / 2)}px`,
       transform: `translate(${(index * width / (events.length - 1)) - 2 * index-1}px,${yOffset * (Math.round(height / 2))-2}px)`
     }
 
+    let styleDescription = {
+      transform: `translate(-50%,${yOffset ==0 ? '100%' : '-100%'})`
+
+    }
+
     eventDivs.push(
       <div style={style} className="block w-[2px] bg-black">
-        <div className={`my-2 h-full -translate-x-1/2	w-fit flex flex-col justify-start text-center ${yOffset == 0 ? '' : '-'}translate-y-full`}>
+        <div style={styleDescription} className={`my-2 h-full w-fit flex flex-col justify-start text-center`}>
           <h2 className="text-lg font-bold w-fit whitespace-nowrap	" >{event.title}</h2>
           <h4 className="text-xs">{event.date}</h4>
           <p className="text-xs ">
