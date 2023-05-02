46b996a7ccc5ffc5fda65f60e9f7bd7b3c2f5ba8
Miguel Lopes <lopesazevedom@gmail.com>
2023-05-02 18:11:45 +0100

    fix wordcloud script

--- data-analysis/wordclouds.py
+++ data-analysis/wordclouds.py
@@ -3,7 +3,7 @@ from nltk.corpus import stopwords
 from collections import Counter
 from os import system
 
-path = "words/"
+path = "./words/"
 
 def import_refined_data(group):
     df = pd.read_json("data/" + group + '.json')
@@ -27,9 +27,13 @@ def plot_wordcloud(df_without_stopwords, group):
         topic_words = [x.lower() for x in topic_words if x.lower()
                        not in stopwords.words("portuguese")]
         word_count_dict = dict(Counter(topic_words))
-        final_dict.update(word_count_dict)
+        for w in word_count_dict:
+            if(w in final_dict):
+                final_dict[w]+=word_count_dict[w]
+            else:
+                final_dict[w]=word_count_dict[w]
 
-    final_dict_sorted = sorted(final_dict, final_dict.get, reverse=True)
+    final_dict_sorted = sorted(final_dict,key=final_dict.get, reverse=True)
     final_dict_sorted = final_dict_sorted[0:50]
 
     f = open(path + group + ".txt", 'w')
