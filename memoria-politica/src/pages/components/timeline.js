c25abcc015b71d02f56f9d1f9a51d4ffecc40802
Maria Carneiro <mariajcarneiro56@gmail.com>
2023-05-03 20:02:51 +0100

    some timeline changes

--- memoria-politica/src/pages/components/timeline.js
+++ memoria-politica/src/pages/components/timeline.js
@@ -11,12 +11,12 @@ const Timeline = ({ width, height, party }) => {
     const yOffset = -1 * ((Number(key) - 1) % 2)
 
     let style = {
-      height: `${Math.round(height / 3)}px`,
-      transform: `translate(${((Number(key) - 1) * width / (Object.keys(data).length - 1)) - 2 * Number(key)}px,${yOffset * (Math.round(height / 3)) - 2}px)`
+      height: `${Math.round(height / 6)}px`,
+      transform: `translate(${((Number(key) - 1) * width / (Object.keys(data).length - 1)) - 2 * Number(key)}px,${yOffset * (Math.round(height / 6)) - 2}px)`
     }
 
     let styleDescription = {
-      transform: `translate(-50%,${yOffset == 0 ? '50%' : '-100%'})`
+      transform: `translate(-50%,${yOffset == 0 ? '30%' : '-100%'})`
     }
 
     console.log(event.description)
@@ -24,10 +24,10 @@ const Timeline = ({ width, height, party }) => {
     eventDivs.push(
       event ? 
       <div key={key} style={style} className="block w-[2px] bg-black">
-        <a href={event.link} style={styleDescription} className={`h-48 rounded-full hover:opacity-75 hover:text-white text-white bg-black w-48 flex flex-col justify-center text-center`}>
-          <h2 className="text-lg font-bold whitespace-nowrap" >{event.title}</h2>
-          <h4 className="text-xs">{event.date}</h4>
-          <p className="text-xs ">
+        <a href={event.link} style={styleDescription} className={`h-64 rounded-full hover:opacity-75 hover:text-white text-white bg-black w-64 px-2 flex flex-col justify-center text-center`}>
+          <h4>{event.date}</h4>
+          <h2 className="text-xl font-bold" >{event.title}</h2>
+          <p className="text-xs px-3 ">
             {event.description}
           </p>
         </a>
@@ -38,9 +38,9 @@ const Timeline = ({ width, height, party }) => {
 
   return (
 
-    <div style={{ width: `${width}px`, height: `${height}px`, "paddingTop": `${Math.round(height / 3)}px` }} className="my-40">
+    <div style={{ width: `${width}px`, height: `${height}px`, "paddingTop": `${Math.round(height / 2)}px` }} className="my-40">
       <div className="h-[2px] w-full bg-black"></div>
-      <div className="flex">
+      <div className="flex items-center">
         {eventDivs}
       </div>
     </div>
