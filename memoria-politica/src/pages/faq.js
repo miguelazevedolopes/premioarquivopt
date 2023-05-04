29667a2dee20b3d7c933fc68cc55e2ace13e86a9
Miguel Lopes <lopesazevedom@gmail.com>
2023-05-04 02:12:31 +0100

    updating descriptions

--- memoria-politica/src/pages/faq.js
+++ memoria-politica/src/pages/faq.js
@@ -12,36 +12,36 @@ const faqs = [
     id: 2,
     title: "Como é constituído o corpus?",
     description:
-      "O <strong>corpus</strong> final utilizado está dividido em diversos <strong>corpus</strong> diferentes, correspondendo cada um deles a um dos partidos políticos presentes na Assembleia Portuguesa atualmente.<p>Estes <strong>corpus</strong> foram criados com base nos ficheiros HTML obtidos através do Arquivo.pt que é um repositório nacional que guarda informação sobre páginas WEB Portuguesas desde 1996.</p><p> Dado que cada partido tem, não só uma data de criação diferente, como também diferentes presenças online, cada corpus abarca períodos temporais diferentes (<strong>PSD</strong> 1999-Final de 2020, <strong>PS</strong> 1996-Final de 2020, <strong>Chega</strong> 2019-Final de 2020, <strong>IL</strong> 2017-Final de 2020, <strong>PCP</strong> 1996-Final de 2020, <strong>BE</strong> 2003-Final de 2020, <strong>Livre</strong> 2015-Final de 2020, <strong>PAN</strong> 2013-Final de 2020).</p>"
+      "O <strong>corpus</strong> final utilizado está dividido em diversos <strong>corpus</strong> diferentes, correspondendo cada um deles a um dos partidos políticos presentes na Assembleia Portuguesa atualmente.<p>Estes <strong>corpus</strong> foram criados com base nos ficheiros HTML obtidos através do Arquivo.pt que é um repositório nacional que guarda informação sobre páginas WEB Portuguesas desde 1996.</p><p> Dado que cada partido tem, não só uma data de criação diferente, como também diferentes presenças online, cada corpus abarca períodos temporais diferentes (<strong>PSD</strong> 1999-2020, <strong>PS</strong> 1996-2020, <strong>Chega</strong> 2019-2020, <strong>IL</strong> 2017-2020, <strong>PCP</strong> 1996-2020, <strong>BE</strong> 2003-2020, <strong>Livre</strong> 2015-2020, <strong>PAN</strong> 2013-2020).</p>"
 
   }
 ];
 
-  
+
 export default function SearchPage() {
 
-      
-    return (
-      <>
-        <Head>
-          <title >FAQ</title>
-          <meta name="description" content="Um arquivo das páginas web dos partidos políticos Portugueses." />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-          <div className='flex-col p-0 sm:m-auto'>
-          <div id="faq" className='flex flex-col mx-12'>
-              <h2 className='text-3xl lg:text-4xl font-extrabold inline-block select-none mt-10 flex justify-center items-center'>FAQ</h2>
-              <div className='mt-5'>
-                {faqs.map((result) => (
-                  <div key={result.id} className="bg-white p-4 my-2 shadow-lg">
-                    <h1 className="text-3xl font-bold mb-2">{result.title}</h1>
-                    <div className="text-gray-700"dangerouslySetInnerHTML={{ __html: result.description }}/>                 
-                  </div>
-                ))}
+
+  return (
+    <>
+      <Head>
+        <title >FAQ</title>
+        <meta name="description" content="Um arquivo das páginas web dos partidos políticos Portugueses." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='flex-col p-0 sm:m-auto'>
+        <div id="faq" className='flex flex-col mx-12'>
+          <h2 className='text-3xl lg:text-4xl font-extrabold inline-block select-none mt-10 flex justify-center items-center'>FAQ</h2>
+          <div className='mt-5'>
+            {faqs.map((result) => (
+              <div key={result.id} className="bg-white p-4 my-2 shadow-lg">
+                <h1 className="text-3xl font-bold mb-2">{result.title}</h1>
+                <div className="text-gray-700" dangerouslySetInnerHTML={{ __html: result.description }} />
               </div>
+            ))}
           </div>
-      </div>   
-    </> 
-      )
-  }
+        </div>
+      </div>
+    </>
+  )
+}
