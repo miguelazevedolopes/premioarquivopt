68948e0bfbd26dee367b95755dad8cf6ace62db2
Maria Carneiro <mariajcarneiro56@gmail.com>
2023-05-04 05:47:09 +0100

    text

--- memoria-politica/src/pages/faq.js
+++ memoria-politica/src/pages/faq.js
@@ -14,7 +14,20 @@ const faqs = [
     description:
       "O <strong>corpus</strong> final utilizado está dividido em diversos <strong>corpus</strong> diferentes, correspondendo cada um deles a um dos partidos políticos presentes na Assembleia Portuguesa atualmente.<p>Estes <strong>corpus</strong> foram criados com base nos ficheiros HTML obtidos através do Arquivo.pt que é um repositório nacional que guarda informação sobre páginas WEB Portuguesas desde 1996.</p><p> Dado que cada partido tem, não só uma data de criação diferente, como também diferentes presenças online, cada corpus abarca períodos temporais diferentes (<strong>PSD</strong> 1999-2020, <strong>PS</strong> 1996-2020, <strong>Chega</strong> 2019-2020, <strong>IL</strong> 2017-2020, <strong>PCP</strong> 1996-2020, <strong>BE</strong> 2003-2020, <strong>Livre</strong> 2015-2020, <strong>PAN</strong> 2013-2020).</p>"
 
-  }
+  },
+  {
+    id: 3,
+    title: "Porque é que alguns partidos não estão incluídos?",
+    description:
+      "A nossa escolha dos partidos a incluir baseou-se na relevância atual para a população portuguesa: é importante sabermos e conhecermos a história de quem nos representa. No entanto, futuramente, planeamos em incluir o resto dos partidos existentes atualmente e, possivelmente, novos partidos que possam surgir."
+  },
+  {
+    id: 3,
+    title: "A Análise de Sentimento é confiável?",
+    description:
+      "A Análise de Sentimento é baseada em modelos de linguagem pré-treinados e aplicada aos textos retirados do Arquivo.pt. Para mais informação acerca da confiança do modelo, visite este <a href='https://textblob.readthedocs.io/en/dev/quickstart.html#sentiment-analysis'>link.</a>"
+  },
+
 ];
 
 
@@ -35,7 +48,7 @@ export default function SearchPage() {
           <div className='mt-5'>
             {faqs.map((result) => (
               <div key={result.id} className="bg-white p-4 my-2 shadow-lg">
-                <h1 className="text-3xl font-bold mb-2">{result.title}</h1>
+                <h1 className="text-3xl font-bold mb-4">{result.title}</h1>
                 <div className="text-gray-700" dangerouslySetInnerHTML={{ __html: result.description }} />
               </div>
             ))}
